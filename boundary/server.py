bbac1815391293b4a1250a9c41f660c7443fe4ce
Manu <manu.sfdlv@gmail.com>
2024-04-28 03:45:27 -0300

    Add RabbitMQ integration to boundary

--- boundary/server.py
+++ boundary/server.py
@@ -1,21 +1,27 @@
 import logging
 import signal
 import socket
+from common.middleware import Middleware
 
 MAX_READ_SIZE = 1024
 LENGTH_BYTES = 2
 
 
 class Server():
-    def __init__(self, port: int, backlog: int, destination_queue: str):
+    def __init__(self, port: int, backlog: int, output_queue: str):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind(('', port))
         server_socket.listen(backlog)
         self.server_socket = server_socket
         self.port = port
-        self.destination_queue = destination_queue
+
+        self.broker_connection = Middleware(output_queues=[output_queue])
+        self.broker_connection.send(
+            f"Server started on port {port}".encode())
+        
         logging.info(
-            "Listening for connections and redirecting to %s", destination_queue)
+            "Listening for connections and redirecting to %s", output_queue)
+        
 
     def run(self):
         signal.signal(signal.SIGTERM, self.__graceful_shutdown)
@@ -31,7 +37,7 @@ class Server():
             try:
                 data = receive_line(client_socket)
                 logging.debug("Received line: %s", data.decode().strip())
-                # TODO: Send the data to RabbitMQ
+                self.broker_connection.send(data)
             except ConnectionResetError:
                 logging.info("Connection closed by client")
                 break
