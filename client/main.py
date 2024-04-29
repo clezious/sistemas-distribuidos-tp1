69eb42f90a4e922df966ba48fe30ed4355f4f995
clezious <manuelreberendo@gmail.com>
2024-04-28 22:37:31 -0300

    fix client dataset route

--- client/main.py
+++ client/main.py
@@ -53,8 +53,8 @@ def main():
     config_params = initialize_config()
     initialize_log(config_params["logging_level"])
     client = Client(
-        "./datasets/books_data.csv", 
-        config_params["book_boundary_ip"], 
+        "../datasets/books_data.csv",
+        config_params["book_boundary_ip"],
         config_params["book_boundary_port"])
     client.run()
     logging.info("Sent all books")
