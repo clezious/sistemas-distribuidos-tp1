56e970a2e035825c2217ebc83b3ccfabd2a87a31
clezious <manuelreberendo@gmail.com>
2024-06-07 21:34:16 -0300

    Implemented PersistenceManager into allo statefull services

--- review_mean_aggregator/src/review_mean_aggregator.py
+++ review_mean_aggregator/src/review_mean_aggregator.py
@@ -3,22 +3,29 @@ import logging
 from common.book_stats import BookStats
 from common.eof_packet import EOFPacket
 from common.middleware import Middleware
+from common.packet_decoder import PacketDecoder
+from common.persistence_manager import PersistenceManager
+import json
 
 
 MAX_BOOKS = 10
+BOOK_STATS_KEY = 'book_stats'
 
 
 class ReviewMeanAggregator:
     def __init__(self,
                  input_queues: dict[str, str],
                  output_queues: list[str]):
+        self.persistence_manager = PersistenceManager('../storage/review_mean_aggregator')
         self.middleware = Middleware(
             input_queues=input_queues,
             output_queues=output_queues,
             callback=self._save_stats,
             eof_callback=self._handle_eof,
+            persistence_manager=self.persistence_manager,
         )
         self.books_stats: list[BookStats] = []
+        self._init_state()
 
     def start(self):
         self.middleware.start()
@@ -40,3 +47,10 @@ class ReviewMeanAggregator:
         heapq.heappush(self.books_stats, book_stats)
         if len(self.books_stats) > MAX_BOOKS:
             heapq.heappop(self.books_stats)
+        self.persistence_manager.put(BOOK_STATS_KEY,
+                                     json.dumps([book_stats.encode() for book_stats in self.books_stats]))
+
+    def _init_state(self):
+        state = json.loads(self.persistence_manager.get(BOOK_STATS_KEY) or '[]')
+        self.books_stats = [PacketDecoder.decode(book_stats) for book_stats in state]
+        logging.info(f"State initialized with {[book_stats.encode() for book_stats in self.books_stats]}")
